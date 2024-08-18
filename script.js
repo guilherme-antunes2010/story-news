6c20df08545ccafb79c892c73a29faecb85e0c10
Guilherme-Antunes <guilherme256578@gmail.com>
2024-08-18 17:58:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+function aviso(){
+    var email = document.getElementById("email").value;
+    var nome = document.getElementById("nome").value;
+    var senha = document.getElementById("senha").value;
+
+    alert("As Informações a seguir estão corretas?"+"\n \n"+"Email: "+email+"\n"+"Nome: "+nome+"\n"+"Senha: "+senha);
+    
+}
